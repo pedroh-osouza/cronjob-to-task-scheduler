6f6605ede67ba005d462bce5e1dcb79b4841644c
Pedro <pedro.souza@viacometa.com.br>
2023-03-06 10:44:33 -0400

    chore: update

--- tests/testes.ts
+++ tests/testes.ts
@@ -1,4 +1,4 @@
-import { FacadeCronToTaskScheduler } from "../src/FacadeCronToTaskScheduler";
+import { FacadeCronToTaskScheduler } from "../src/CronToCommand/FacadeCronToTaskScheduler";
 
 let data = FacadeCronToTaskScheduler.convert('00 07 1 1 1', 'TestePedro', 'echo "Teste"');
 
