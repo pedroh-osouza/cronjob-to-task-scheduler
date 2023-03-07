3ae824d87b5ea89cb49b3e4d2014e3b41df58bfd
Pedro <pedro.souza@viacometa.com.br>
2023-03-07 16:17:48 -0400

    feat: all possibilities

--- README.md
+++ README.md
@@ -8,3 +8,48 @@ ROBO STARTA TODO DIA EM UMA OU MAIS HORAS
 ROBO STARTA EM DIAS ESPECIFICOS COM UMA HORA ESPECIFICA
 
 ROBO STARTA EM DIAS ESPECIFICOS COM UMA OU MAIS HORAS
+
+Cada valor separado por espaço em um cron pode ter um dos seguintes valores:
+
+Um número de 0 a 59 (para os minutos e segundos) ou de 0 a 23 (para as horas).
+Um asterisco (*) para indicar que todos os valores são aceitos.
+Um intervalo, no formato valorInicial-valorFinal. Por exemplo, 2-4 indica os valores 2, 3 e 4.
+Uma lista de valores, separados por vírgula. Por exemplo, 1,3,4,7 indica os valores 1, 3, 4 e 7.
+Um intervalo com um incremento, no formato valorInicial-valorFinal/incremento. Por exemplo, 0-23/2 indica os valores 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 e 22.
+Uma lista de valores com incremento, no formato valorInicial,valorInicial+incremento,valorInicial+(2*incremento),.... Por exemplo, 3,6,9,12 indica os valores 3, 6, 9 e 12.
+
+
+# Possibilidades
+
+* * * * *
+1 * * * *
+* 1 * * *
+1 1 * * *
+* * 1 * *
+1 * 1 * *
+* 1 1 * *
+1 1 1 * *
+* * * 1 *
+1 * * 1 *
+* 1 * 1 *
+1 1 * 1 *
+* * 1 1 *
+1 * 1 1 *
+* 1 1 1 *
+1 1 1 1 *
+* * * * 1
+1 * * * 1
+* 1 * * 1
+1 1 * * 1
+* * 1 * 1
+1 * 1 * 1
+* 1 1 * 1
+1 1 1 * 1
+* * * 1 1
+1 * * 1 1
+* 1 * 1 1
+1 1 * 1 1
+* * 1 1 1
+1 * 1 1 1
+* 1 1 1 1
+1 1 1 1 1
--- src/Handlers/SelectScheduleType.ts
+++ src/Handlers/SelectScheduleType.ts
@@ -9,21 +9,39 @@ export class SelectScheduleType
     static select(data: cronData)
     {
         const {minutes, hours, dayOfMonth, month, dayOfWeek} = data;
-
-        // Uma tarefa que executa diariamente em determinada(s) hora(s) e minuto(s), independente do dia do mês, mês e dia da semana
-        const daily: Boolean = (minutes != '*' && hours != '*' && dayOfMonth == '*' && month == '*' && dayOfWeek == '*')
-
-        /*Uma tarefa que executa em dias específicos da semana em determinada(s) hora(s) e minutos(s), independente do dia do mês, mês
-        e dia da semana
-        */
-        const dailySpecificDay: Boolean = (minutes != '*' && hours != '*' && dayOfMonth == '*' && month == '*' && dayOfWeek != '*')
-
-        /*Uma tarefa que executa em determinado(s) minuto(s), independente da hora, dia do mes, mes e dia da semana*/
+        const allMinutes: Boolean = (minutes == '*' && hours == '*' && dayOfMonth == '*' && month == '*' && dayOfWeek == '*')
         const specificMinutes: Boolean = (minutes != '*' && hours == '*' && dayOfMonth == '*' && month == '*' && dayOfWeek == '*')
-
-        /*Uma tarefa que executa em determinada(s) hora(s), independente do minuto, dia do mes, mes e dia da semana*/
         const specificHour: Boolean = (minutes == '*' && hours != '*' && dayOfMonth == '*' && month == '*' && dayOfWeek == '*')
-
+        const daily: Boolean = (minutes != '*' && hours != '*' && dayOfMonth == '*' && month == '*' && dayOfWeek == '*')
+        const specificDayOfMonth: Boolean = (minutes == '*' && hours == '*' && dayOfMonth != '*' && month == '*' && dayOfWeek == '*')
+        const minuteDayOfMonth: Boolean = (minutes != '*' && hours == '*' && dayOfMonth != '*' && month == '*' && dayOfWeek == '*')
+        const hourDayOfMonth: Boolean = (minutes == '*' && hours != '*' && dayOfMonth != '*' && month == '*' && dayOfWeek == '*')
+        const minuteHourDayOfMonth: Boolean = (minutes != '*' && hours != '*' && dayOfMonth != '*' && month == '*' && dayOfWeek == '*')
+        const specificMonth: Boolean = (minutes == '*' && hours == '*' && dayOfMonth == '*' && month != '*' && dayOfWeek == '*')
+        const minuteMonth: Boolean = (minutes != '*' && hours == '*' && dayOfMonth == '*' && month != '*' && dayOfWeek == '*')
+        const hourMonth: Boolean = (minutes == '*' && hours != '*' && dayOfMonth == '*' && month != '*' && dayOfWeek == '*')
+        const minuteHourMonth: Boolean = (minutes != '*' && hours != '*' && dayOfMonth == '*' && month != '*' && dayOfWeek == '*')
+        const dayOfMonthMonth: Boolean = (minutes == '*' && hours == '*' && dayOfMonth != '*' && month != '*' && dayOfWeek == '*')
+        const minuteDayOfMonthMonth: Boolean = (minutes != '*' && hours == '*' && dayOfMonth != '*' && month != '*' && dayOfWeek == '*')
+        const hourDayOfMonthMonth: Boolean = (minutes == '*' && hours != '*' && dayOfMonth != '*' && month != '*' && dayOfWeek == '*')
+        const hourMinuteDayOfMonthMonth: Boolean = (minutes != '*' && hours != '*' && dayOfMonth != '*' && month != '*' && dayOfWeek == '*')
+        const specificDayOfWeek: Boolean = (minutes == '*' && hours == '*' && dayOfMonth == '*' && month == '*' && dayOfWeek != '*')
+        const minuteDayOfWeek: Boolean = (minutes != '*' && hours == '*' && dayOfMonth == '*' && month == '*' && dayOfWeek != '*')
+        const hourDayOfWeek: Boolean = (minutes == '*' && hours != '*' && dayOfMonth == '*' && month == '*' && dayOfWeek != '*')
+        const dailySpecificDay: Boolean = (minutes != '*' && hours != '*' && dayOfMonth == '*' && month == '*' && dayOfWeek != '*')
+        const dayOfMonthDayOfWeek: Boolean = (minutes == '*' && hours == '*' && dayOfMonth != '*' && month == '*' && dayOfWeek != '*')
+        const minuteDayOfMonthDayOfWeek: Boolean = (minutes != '*' && hours == '*' && dayOfMonth != '*' && month == '*' && dayOfWeek != '*')
+        const hourDayOfMonthDayOfWeek: Boolean = (minutes == '*' && hours != '*' && dayOfMonth != '*' && month == '*' && dayOfWeek != '*')
+        const minuteHourDayOfMonthDayOfWeek : Boolean = (minutes != '*' && hours != '*' && dayOfMonth != '*' && month == '*' && dayOfWeek != '*')
+        const monthDayOfWeek: Boolean = (minutes == '*' && hours == '*' && dayOfMonth == '*' && month != '*' && dayOfWeek != '*')
+        const minuteMonthDayOfWeek: Boolean = (minutes != '*' && hours == '*' && dayOfMonth == '*' && month != '*' && dayOfWeek != '*')
+        const hourMonthDayOfWeek: Boolean = (minutes == '*' && hours != '*' && dayOfMonth == '*' && month != '*' && dayOfWeek != '*')
+        const minuteHourMonthDayOfWeek: Boolean = (minutes != '*' && hours != '*' && dayOfMonth == '*' && month != '*' && dayOfWeek != '*')
+        const dayOfMonthMonthDayOfWeek: Boolean = (minutes == '*' && hours == '*' && dayOfMonth != '*' && month != '*' && dayOfWeek != '*')
+        const minuteDayOfMonthMonthDayOfWeek: Boolean = (minutes != '*' && hours == '*' && dayOfMonth != '*' && month != '*' && dayOfWeek != '*')
+        const hourDayOfMonthMonthDayOfWeek: Boolean = (minutes == '*' && hours != '*' && dayOfMonth != '*' && month != '*' && dayOfWeek != '*')
+        const specificAll: Boolean = (minutes != '*' && hours != '*' && dayOfMonth != '*' && month != '*' && dayOfWeek != '*')
+        
         if(daily) return Daily.handle(minutes, hours);
         if(dailySpecificDay) return DailySpecificDay.handle(minutes, hours, dayOfWeek);
         if(specificMinutes) return SpecificMinute.handle(minutes);
