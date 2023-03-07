2524a1475b1739f5deef086ad8a46e8bd9090fef
Pedro <pedro.souza@viacometa.com.br>
2023-03-07 14:13:59 -0400

    feat: specific hour

--- src/Handlers/SchedulesTypes/Daily.ts
+++ src/Handlers/SchedulesTypes/Daily.ts
@@ -4,7 +4,7 @@ export class Daily
 {
     static handle(minutes: string|string[], hours: string|string[])
     {
-        const baseSchedule = '/sc daily /ST'
+        const baseSchedule = '/sc daily /st'
         
         let startTimes = Extract.startTime(minutes, hours);
         let schedules: string[] = [];
--- src/Handlers/SchedulesTypes/SpecificHour.ts
+++ src/Handlers/SchedulesTypes/SpecificHour.ts
@@ -2,9 +2,19 @@ export class SpecificHour
 {
     static handle(hour: string|string[])
     {
+        const baseSchedule = '/sc minute /mo 1 /st'
+
         if(!Array.isArray(hour))
         {
-            return;
+            return `${baseSchedule} ${hour}:00 /et ${hour}:59`
         }
+
+        let schedules: string[] = [];
+
+        hour.forEach((value)=>{
+            schedules.push(`${baseSchedule} ${value}:00 /et ${value}:59`)
+        })
+
+        return schedules;
     }
 }
