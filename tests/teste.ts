00f31ef203a30ade056711d520bd87f1d40ac940
Pedro <pedro.souza@viacometa.com.br>
2023-03-09 15:30:49 -0400

    chore: update

--- tests/teste.ts
+++ tests/teste.ts
@@ -1,3 +1,6 @@
+import { CronToTaskSchedule } from "../src/CronToTaskSchedule";
 import { CronToXml } from "../src/CronToXml/FacadeCronToXml";
 
+// CronToTaskSchedule.convert('meuXml', '00,30 07,17 * * *', 'notepad.exe')
 CronToXml.convert('meuXml', '00,30 07,17 * * *', 'notepad.exe')
+
