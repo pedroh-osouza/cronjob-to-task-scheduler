5685024a2e86c5c0ac1a6ff0989428917e385854
Pedro <pedro.souza@viacometa.com.br>
2023-03-10 17:27:31 -0400

    chore: update

--- src/Handlers/SchedulesTypes/MonthlyDayOfWeek.ts
+++ src/Handlers/SchedulesTypes/MonthlyDayOfWeek.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 import { CronData } from "../../interfaces/CronData";
-import { CalendarTrigger } from "../../interfaces/ScheduleXmlObject";
+import { CalendarTrigger, Triggers } from "../../interfaces/ScheduleXmlObject";
 import { MonthDayOfWeek } from "../MonthDayOfWeek";
 
 export class MonthlyDayOfWeek
@@ -103,9 +103,69 @@ export class MonthlyDayOfWeek
         };
     }
 
-    private static hour(cronData: CronData)
+    private static hour(cronData: CronData): Triggers
     {
+        const now = moment();
+        const scheduleByMonthDayOfWeek = MonthDayOfWeek.getScheduleMonthDayOfWeek(cronData);
 
+        if(!Array.isArray(cronData.hours))
+        {
+            now.set({hour: Number(cronData.hours), minute: 0, second: 0})
+            return {
+                CalendarTrigger: {
+                    Repetition: {
+                        Interval: {
+                            _text: 'PT1M'
+                        },
+                        Duration:{
+                            _text: 'PT1H'
+                        },
+                        StopAtDurationEnd: {
+                            _text: false
+                        },
+                    },
+                    Enabled: {
+                        _text: true
+                    },
+                    StartBoundary: {
+                        _text: now.format('YYYY-MM-DDTHH:mm:ssZ')
+                    },
+                    ScheduleByMonthDayOfWeek: scheduleByMonthDayOfWeek
+                }
+            }
+        }
+
+        let calendarTriggers: CalendarTrigger[] = [];
+        for(let i = 0; i < cronData.hours.length; i++)
+        {
+            now.set({hour: Number(cronData.hours[i]), minute: 0, second: 0})
+            const trigger: CalendarTrigger = {
+                Repetition: {
+                    Interval: {
+                        _text: 'PT1M'
+                    },
+                    Duration:{
+                        _text: 'PT1H'
+                    },
+                    StopAtDurationEnd: {
+                        _text: false
+                    },
+                },
+                Enabled: {
+                    _text: true
+                },
+                StartBoundary: {
+                    _text: now.format('YYYY-MM-DDTHH:mm:ssZ')
+                },
+                ScheduleByMonthDayOfWeek: scheduleByMonthDayOfWeek
+            }
+
+            calendarTriggers.push(trigger)
+        }
+        
+        return {
+            CalendarTrigger: calendarTriggers
+        };
     }
 
     private static minuteHour(cronData: CronData)
--- tests/teste.ts
+++ tests/teste.ts
@@ -1,3 +1,3 @@
 import { CronToXml } from "../src/FacadeCronToXml"
 
-CronToXml.convert('meuXml', '1 * * 1 1-5', 'notepad.exe')
+CronToXml.convert('meuXml', '* 10 * 1 1-5', 'notepad.exe')
