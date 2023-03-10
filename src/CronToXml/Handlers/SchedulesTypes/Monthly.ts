622410ead4059c7b458706491b911356e553887e
Pedro <pedro.souza@viacometa.com.br>
2023-03-10 13:11:30 -0400

    feat: tarefa mensal definindo hora

--- src/CronToXml/Handlers/SchedulesTypes/Monthly.ts
+++ src/CronToXml/Handlers/SchedulesTypes/Monthly.ts
@@ -8,6 +8,8 @@ export class Monthly
     {
         if(cronData.minutes == '*' && cronData.hours == '*' && cronData.daysOfMonths != '*' && cronData.months == '*' && cronData.daysOfWeeks == '*') return this.dayOfMonth(cronData);
         if(cronData.minutes != '*' && cronData.hours == '*' && cronData.daysOfMonths != '*' && cronData.months == '*' && cronData.daysOfWeeks == '*') return this.minuteDayOfMonth(cronData)
+        if(cronData.minutes == '*' && cronData.hours != '*' && cronData.daysOfMonths != '*' && cronData.months == '*' && cronData.daysOfWeeks == '*') return this.hourDayOfMonth(cronData)
+
         return {
 
         };
@@ -39,7 +41,7 @@ export class Monthly
         }
     }
 
-    private static minuteDayOfMonth(cronData: CronData)
+    private static minuteDayOfMonth(cronData: CronData): Triggers
     {
         const now = moment();
         const scheduleByMonth = DaysOfMonth.getScheduleMonth(cronData);
@@ -76,6 +78,71 @@ export class Monthly
             const trigger: CalendarTrigger = {
                 Repetition: {
                     Interval: {
+                        _text: 'PT1H'
+                    },
+                    StopAtDurationEnd: {
+                        _text: false
+                    },
+                },
+                Enabled: {
+                    _text: true
+                },
+                StartBoundary: {
+                    _text: now.format('YYYY-MM-DDTHH:mm:ssZ')
+                },
+                ScheduleByMonth: scheduleByMonth
+            }
+
+            calendarTriggers.push(trigger)
+        }
+        
+        return {
+            CalendarTrigger: calendarTriggers
+        };
+    }
+
+    private static hourDayOfMonth(cronData: CronData): Triggers
+    {
+        const now = moment();
+        const scheduleByMonth = DaysOfMonth.getScheduleMonth(cronData);
+
+        if(!Array.isArray(cronData.hours))
+        {
+            now.set({hour: Number(cronData.hours), minute: 0, second: 0})
+            return {
+                CalendarTrigger: {
+                    Repetition: {
+                        Interval: {
+                            _text: 'PT1M'
+                        },
+                        Duration:{
+                            _text: 'PT1H'
+                        },
+                        StopAtDurationEnd: {
+                            _text: false
+                        },
+                    },
+                    Enabled: {
+                        _text: true
+                    },
+                    StartBoundary: {
+                        _text: now.format('YYYY-MM-DDTHH:mm:ssZ')
+                    },
+                    ScheduleByMonth: scheduleByMonth
+                }
+            }
+        }
+
+        let calendarTriggers: CalendarTrigger[] = [];
+        for(let i = 0; i < cronData.hours.length; i++)
+        {
+            now.set({hour: Number(cronData.hours[i]), minute: 0, second: 0})
+            const trigger: CalendarTrigger = {
+                Repetition: {
+                    Interval: {
+                        _text: 'PT1M'
+                    },
+                    Duration:{
                         _text: 'PT1H'
                     },
                     StopAtDurationEnd: {
