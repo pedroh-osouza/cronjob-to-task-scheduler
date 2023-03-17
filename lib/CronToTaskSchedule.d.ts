680962d9cbd7e5dad8ff67a1ec7c7f7ada1347e6
Pedro <pedro.souza@viacometa.com.br>
2023-03-17 16:48:20 -0400

    feat: delete task with name

--- lib/CronToTaskSchedule.d.ts
+++ lib/CronToTaskSchedule.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
 export declare class CronToTaskSchedule {
     static convert(taskName: string, cronExpression: string, taskRun: string): boolean;
+    static deleteTask(taskName: string): Buffer;
 }
